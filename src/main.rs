4e96b1248e42dac1009382a3063c757b1d7e465b
Sergey Potapov <blake131313@gmail.com>
2018-08-14 21:29:57 +0200

    Use openssl_probe to setup openssl for static build

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 extern crate hail;
+extern crate openssl_probe;
 
 fn main() {
+    // Necessary to make OpenSSL work in a static build.
+    // See: https://github.com/emk/rust-musl-builder#making-openssl-work
+    openssl_probe::init_ssl_cert_env_vars();
+
     hail::run();
 }
